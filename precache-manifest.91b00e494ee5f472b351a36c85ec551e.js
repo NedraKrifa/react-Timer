7651342b4b2dbf8b671accec3d16b5488c24cc1a
Nedra Krifa <nedrakrifa@gmail.com>
2020-06-03 18:24:11 +0200

    Updates

---
+++ precache-manifest.91b00e494ee5f472b351a36c85ec551e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca07958fafcef7898ae4bfbb8cc3d568",
+    "url": "/react-Timer/index.html"
+  },
+  {
+    "revision": "326735365ecd8a5d2c6f",
+    "url": "/react-Timer/static/css/main.376a540e.chunk.css"
+  },
+  {
+    "revision": "690bafcbab523185f4bd",
+    "url": "/react-Timer/static/js/2.73983b0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Timer/static/js/2.73983b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326735365ecd8a5d2c6f",
+    "url": "/react-Timer/static/js/main.93ea0789.chunk.js"
+  },
+  {
+    "revision": "90e2316e8fc766673717",
+    "url": "/react-Timer/static/js/runtime-main.d9118eed.js"
+  }
+]);
